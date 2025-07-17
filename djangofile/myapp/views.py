b81ac18a7a5eed09eebb394102c589a4c6022ec8
luizaclemos <luizaclemos.adv@gmail.com>
2025-07-17 07:15:57 -0300

    creating the second html file

--- djangofile/myapp/views.py
+++ djangofile/myapp/views.py
@@ -4,3 +4,23 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def letter(request):
+    if request.method == 'POST':
+        context = {
+            'name': request.POST.get('name'),
+            'profession': request.POST.get('profession'),
+            'cpf': request.POST.get('cpf'),
+            'identity_card': request.POST.get('identity_card'),
+            'nationality': request.POST.get('nationality'),
+            'street': request.POST.get('street'),
+            'city': request.POST.get('city'),
+            'state': request.POST.get('state'),
+            'zipcode': request.POST.get('zipcode'),
+            'country': request.POST.get('country'),
+            'email': request.POST.get('email'),
+            'maritalstatus': request.POST.get('maritalstatus'),
+            'powers': request.POST.getlist('powers[]')
+        }
+        return render(request, 'letter.html', context)
+    return render(request, 'index.html')
