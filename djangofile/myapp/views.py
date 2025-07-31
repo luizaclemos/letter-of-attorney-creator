ec1256a6f751f32fc9831307988ec9653c059fc4
luizaclemos <luizaclemos.adv@gmail.com>
2025-07-31 18:11:50 -0300

    configuring pdf formatting

--- djangofile/myapp/views.py
+++ djangofile/myapp/views.py
@@ -1,7 +1,11 @@
+from django.conf import settings
 from django.shortcuts import render
 from django.template.loader import get_template
 from django.http import HttpResponse
+from django.contrib.staticfiles import finders
 from xhtml2pdf import pisa
+import os
+
 
 
 # Create your views here.
@@ -49,16 +53,19 @@ def download_pdf(request):
             'email': request.POST.get('email'),
             'maritalstatus': request.POST.get('maritalstatus'),
             'powers': request.POST.getlist('powers[]'),
-            'is_pdf': True
+            'is_pdf': True,
         }
 
         template = get_template('letter.html')
         html = template.render(context)
 
+        css_path = os.path.join(settings.STATICFILES_DIRS[0], 'pdf.css')
+
         response = HttpResponse(content_type='application/pdf')
         response['Content-Disposition'] = 'attachment; filename="letter.pdf"'
 
-        pisa_status = pisa.CreatePDF(html, dest=response)
+        with open(css_path, 'r') as css_file:
+            pisa_status = pisa.CreatePDF(html, dest=response, link_callback=link_callback, default_css=css_file.read())
 
         if pisa_status.err:
             return HttpResponse('We had an error while generating your PDF', status=500)
@@ -66,3 +73,13 @@ def download_pdf(request):
         return response
 
     return HttpResponse("Invalid request", status=400)
+
+def link_callback(uri, rel):
+    if uri.startswith(settings.STATIC_URL):
+        path = os.path.join(settings.STATICFILES_DIRS[0], uri.replace(settings.STATIC_URL, ''))
+    else:
+        path = finders.find(uri)
+        if not path:
+            raise Exception(f'Arquivo estático não encontrado: {uri}')
+    return path
+
